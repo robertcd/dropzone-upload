bf80a30d4e7a638df354fcd0237e6553442bebe9
Robert Schoeftner <robert.schoeftner@helvetia.ch>
2021-03-11 14:10:19 +0000

    adjusted cors filter, now working with ui.R

--- api/api.R
+++ api/api.R
@@ -4,9 +4,16 @@ library(plumber)
 
 
 #* @filter cors
-function(res) {
-  res$setHeader('Access-Control-Allow-Origin','*')
-  plumber::forward()
+function(req, res) {
+  res$setHeader("Access-Control-Allow-Origin", "*")
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$setHeader("Access-Control-Allow-Methods","*")
+    res$setHeader("Access-Control-Allow-Headers", req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS)
+    res$status <- 200 
+    return(list())
+  } else {
+    plumber::forward()
+  }
 }
 
 #* Plot a histogram
